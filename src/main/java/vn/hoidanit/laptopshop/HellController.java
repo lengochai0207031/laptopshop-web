34ead055a2fe0429912d16603a2f1f9d83a140fb
lengochai0207031 <lhai2274@gmail.com>
2024-04-18 03:00:58 +0700

    xong chạy mẫu đầu tiên

---
+++ src/main/java/vn/hoidanit/laptopshop/HellController.java
@@ -0,0 +1,11 @@
+package vn.hoidanit.laptopshop;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HellController {
+@GetMapping("/")
+public String index(){
+    return "Hello World form SpringBoot ?";
+}
+}
