35078c1abc16bc28b8be1a033e3b22afe6a89d5c
lengochai0207031 <lhai2274@gmail.com>
2024-04-19 02:28:06 +0700

    Spring Security

--- src/main/java/vn/hoidanit/laptopshop/HellController.java
+++ src/main/java/vn/hoidanit/laptopshop/HellController.java
@@ -2,10 +2,23 @@ package vn.hoidanit.laptopshop;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class HellController {
 @GetMapping("/")
 public String index(){
-    return "Hello World form SpringBoot ?";
+    return "Hello World form SpringBoot cố lên nha hải ?";
 }
+
+@GetMapping("/user")
+public String userPage() {
+    return "Only users can access this page.";
+}
+
+@GetMapping("/admin")
+public String admiPage() {
+    return "Only admin can access this page.";
+}
+
+
 }
