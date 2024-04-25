a1bbe33f835e3978f51f0acfd9e08ee5161f0208
lengochai0207031 <lhai2274@gmail.com>
2024-04-25 23:13:31 +0700

    tạo create width jsp

--- src/main/java/vn/hoidanit/laptopshop/controllers/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controllers/UserController.java
@@ -2,11 +2,16 @@ package vn.hoidanit.laptopshop.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 
+
 import vn.hoidanit.laptopshop.services.UserService;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
@@ -21,12 +26,22 @@ public class UserController {
 
     @RequestMapping("/")
 
-    public String getHome() {
+    public String getHome(Model model) {
       String test  = this.userService.handleHellos();
+      model.addAttribute("eric", test);
         return "hello";
 
         // ở đay return ko có duôi nha cmmm 
     
+}
+    @RequestMapping("/admin/user")
+    public String getUser(Model model) {
+        String test  = this.userService.handleHellos();
+        model.addAttribute("eric", test);
+        return "admin/user/create";
+   
+    
+ 
 }
 }
 
---
+++ src/main/java/vn/hoidanit/laptopshop/domain/User.java
@@ -0,0 +1,54 @@
+package vn.hoidanit.laptopshop.domain;
+
+public class User {
+    private long id;
+    private String email ; 
+    private String passWord ;
+    private String fullName ; 
+    private String address ;
+    private String phone ;
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassWord() {
+        return passWord;
+    }
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+    public String getFullName() {
+        return fullName;
+    }
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", email=" + email + ", passWord=" + passWord + ", fullName=" + fullName
+                + ", address=" + address + ", phone=" + phone + "]";
+    }
+
+    
+
+}
